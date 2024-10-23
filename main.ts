790e06ad7f6990e246827f68b0f48de466d6c8eb
Nico-Sellitto <nico.sellitto@mths.ca>
2024-10-23 10:52:26 -0400

    coded

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nico Sellitto
+ * Created on: Oct 2024
+ * This program turns a servo
 */
 
-basic.showString('Hello, World!')
+const servoNumber1 = robotbit.Servos.S1
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// Turns servo 0 dergrees 
+input.onButtonPressed(Button.A, function() {
+    robotbit.Servo(servoNumber1, 0)
+    basic.clearScreen()
+    basic.showString('0')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Happy)
+})
+
+// Turns servo 180 dergrees 
+input.onButtonPressed(Button.B, function () {
+    robotbit.Servo(servoNumber1, 180)
+    basic.clearScreen()
+    basic.showString('180')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Happy)
+})
